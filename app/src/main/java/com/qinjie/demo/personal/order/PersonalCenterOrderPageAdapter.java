be9ef59bea79a61030de7025d15ae102e79e9732
qinjie <1768923041@qq.com>
2020-06-19 10:35:26 +0800

    修改任务

--- app/src/main/java/com/qinjie/demo/personal/order/PersonalCenterOrderPageAdapter.java
+++ app/src/main/java/com/qinjie/demo/personal/order/PersonalCenterOrderPageAdapter.java
@@ -19,7 +19,6 @@ import static com.qinjie.demo.utils.ListViewUtil.setListViewHeightBasedOnChildre
 
 
 public class PersonalCenterOrderPageAdapter extends BaseAdapter {
-
     public PersonalCenterOrderPageAdapter(Context context, List<OrderInfo> list) {
         this.mContext = context;
         this.layoutInflater = LayoutInflater.from(context);
@@ -68,34 +67,46 @@ public class PersonalCenterOrderPageAdapter extends BaseAdapter {
          * 按钮删除
          */
         Button personal_center_order_page_list_button_delete;
+        /**
+         * 支付
+         */
+        Button personal_center_order_page_list_button_pay;
     }
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder viewHolde = new ViewHolder();
-        if(convertView == null){
+//        if(convertView == null){
             convertView = layoutInflater.inflate(R.layout.fragment_personal_center_order_page_item, null);
             convertView.setTag(viewHolde);
             viewHolde.personal_center_order_page_list_goods_goods_num_total_price = convertView.findViewById(R.id.personal_center_order_page_list_goods_goods_num_total_price);
             viewHolde.personal_center_order_page_list_store = convertView.findViewById(R.id.personal_center_order_page_list_store);
             viewHolde.personal_center_order_page_list_order_status = convertView.findViewById(R.id.personal_center_order_page_list_order_status);
             viewHolde.personal_center_order_page_list_button_delete = convertView.findViewById(R.id.personal_center_order_page_list_button_delete);
+            viewHolde.personal_center_order_page_list_button_pay = convertView.findViewById(R.id.personal_center_order_page_list_button_pay);
             viewHolde.personal_center_order_page_list_goods = convertView.findViewById(R.id.personal_center_order_page_list_goods);
 
-        }
-        else{
-            viewHolde = (ViewHolder)convertView.getTag();
-        }
+//        }
+//        else{
+//            viewHolde = (ViewHolder)convertView.getTag();
+//        }
         viewHolde.personal_center_order_page_list_goods_goods_num_total_price.setText("共计"+mOrderInfoList.get(position).getGoodsNum()+"件商品，合计"+mOrderInfoList.get(position).getTotalPrice()+"元");
         viewHolde.personal_center_order_page_list_store.setText(mOrderInfoList.get(position).getStoreName());
         viewHolde.personal_center_order_page_list_order_status.setText(mOrderInfoList.get(position).getOrderStatus());
-        viewHolde.personal_center_order_page_list_button_delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder3 = new AlertDialog.Builder(mContext);
-                builder3.setTitle("删除订单？").setNegativeButton("取消", null)
-                        .setPositiveButton("确认", null).setCancelable(false).show();
-            }
-        });
+        if(mOrderInfoList.get(position).getOrderStatus().equals("未支付")){
+            viewHolde.personal_center_order_page_list_button_pay.setVisibility(View.VISIBLE);
+        }
+//        else if(mOrderInfoList.get(position).getOrderStatus().equals("已支付")){
+//            viewHolde.personal_center_order_page_list_button_delete.setVisibility(View.VISIBLE);
+//        }
+
+//        viewHolde.personal_center_order_page_list_button_delete.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                AlertDialog.Builder builder3 = new AlertDialog.Builder(mContext);
+//                builder3.setTitle("删除订单？").setNegativeButton("取消", null)
+//                        .setPositiveButton("确认", null).setCancelable(false).show();
+//            }
+//        });
         viewHolde.personal_center_order_page_list_goods.setAdapter(new PersonalCenterOrderPageGoodsAdapter(mContext, mOrderInfoList.get(position).getGoodsInfos(),layoutInflater ));
         setListViewHeightBasedOnChildren(viewHolde.personal_center_order_page_list_goods);
         return convertView;
